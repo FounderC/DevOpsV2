3c423f005f26abb46db7778e13573081b00cf23b
Founder <141733979+FounderC@users.noreply.github.com>
2024-12-16 21:06:06 +0200

    update

--- vertical_scaling/scriptB.sh
+++ vertical_scaling/scriptB.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 LOG_FILE="scriptB.log"
-TARGET_URL="http://localhost" # nginx LB endpoint
+TARGET_URL="http://localhost:8081/compute" # nginx LB endpoint
 
 while true; do
-    WAIT_TIME=$((RANDOM%6+5)) # випадковий час від 5 до 10 секунд
+    WAIT_TIME=$((RANDOM%1+2)) # випадковий час від 5 до 10 секунд
     echo "$(date) Sending request to $TARGET_URL" | tee -a $LOG_FILE
     curl -s -o /dev/null -w "%{http_code}\n" $TARGET_URL | tee -a $LOG_FILE
     sleep $WAIT_TIME &
